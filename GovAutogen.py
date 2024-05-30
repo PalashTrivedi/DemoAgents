b5017a2616ac42c9c4bcf1bc563cde49e54ce1d6
PalashTrivedi <palash.trivedi05@gmail.com>
2024-05-30 15:27:09 +0530

    Update GovAutogen.py

--- GovAutogen.py
+++ GovAutogen.py
@@ -1,13 +1,9 @@
-# import autogen
-# from autogen.agentchat import AssistantAgent, UserProxyAgent
-# import streamlit as st
-# import asyncio
-# # from SalesAgent import MathUserProxyAgent
-# import pprint
-#
-#
-#
-#
+import autogen
+from autogen.agentchat import AssistantAgent, UserProxyAgent
+import streamlit as st
+import asyncio
+
+
 # # setup page title and description
 # st.set_page_config(page_title="AutoGen Chat app", page_icon="🤖", layout="wide")
 #
@@ -118,76 +114,7 @@ import os
 from autogen.agentchat import AssistantAgent, UserProxyAgent
 import streamlit as st
 import asyncio
-# class TrackableAssistantAgent(AssistantAgent):
-#     """
-#     A custom AssistantAgent that tracks the messages it receives.
-#
-#     This is done by overriding the `_process_received_message` method.
-#     """
-#
-#     def _process_received_message(self, message, sender, silent):
-#         with st.chat_message(sender.name):
-#             st.markdown(message)
-#         return super()._process_received_message(message, sender, silent)
-#
-#
-# class TrackableUserProxyAgent(UserProxyAgent):
-#     """
-#     A custom UserProxyAgent that tracks the messages it receives.
-#
-#     This is done by overriding the `_process_received_message` method.
-#     """
-#
-#     def _process_received_message(self, message, sender, silent):
-#         with st.chat_message(sender.name):
-#             st.markdown(message)
-#         return super()._process_received_message(message, sender, silent)
-#
-#
-#
-#
-# config_list = [
-#     {
-#         "model": "gpt-3.5-turbo",
-#         "api_key": "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",
-#     },
-#
-# ]
-#
-#
-#
-# # agent_with_number = TrackableAssistantAgent(
-# #             "agent_with_number",
-# #             system_message="You are playing a game of guess-my-number."
-# #                            "In the first game, you have the "
-# #                            "number 53 in your mind, and I will try to guess it. "
-# #                            "If I guess too high, say 'too high', if I guess too low, say 'too low'. ",
-# #             llm_config={"config_list": config_list},
-# #             max_consecutive_auto_reply=1,  # maximum number of consecutive auto-replies before asking for human input
-# #             is_termination_msg=lambda msg: "53" in msg["content"],
-# #             # terminate if the number is guessed by the other agent
-# #             human_input_mode="TERMINATE",  # ask for human input until the game is terminated
-# #         )
-# human_proxy = TrackableAssistantAgent(
-#     "human_proxy",
-#     llm_config=False,  # no LLM used for human proxy
-#     human_input_mode="ALWAYS",  # always ask for human input
-# )
-#
-# agent_guess_number = TrackableUserProxyAgent(
-#             "agent_guess_number",
-#             system_message="I have a number in my mind, and you will try to guess it. "
-#                            "If I say 'too high', you should guess a lower number. If I say 'too low', "
-#                            "you should guess a higher number. ",
-#             llm_config={"config_list":config_list},
-#             human_input_mode="NEVER",
-#         )
-# result = human_proxy.initiate_chat(
-#     agent_guess_number,
-#     message="I have a number between 1 and 100. Guess it!",
-# )
-#
-# st.write(result)
+
 
 
 # setup page title and description
